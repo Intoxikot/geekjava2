5be4d366976202ee1e548f251d93ff2086471a5b
koteev <netoxikot@gmail.com>
2019-03-14 22:51:53 +0500

    Реализованы DogCharacterService и HumanCharacterService по аналогии с реализацией в CatCharacterService

--- lesson-01/service/impl/HumanCharacterService.java
+++ lesson-01/service/impl/HumanCharacterService.java
@@ -1,5 +1,6 @@
 package service.impl;
 
+import constants.Constants;
 import valuestype.ActType;
 import animals.CharacterValue;
 import service.CharacterService;
@@ -14,15 +15,30 @@ public class HumanCharacterService implements CharacterService
   @Override
   public CharacterValue getCharacterValue(ActType actType)
   {
-    //TODO
-    return null;
+    CharacterValue characterValue = new CharacterValue();
+    int value = 0;
+    switch (actType) {
+        case JUMP: value = (Constants.Human.Female.POWER + Constants.Human.Male.POWER) / 3;
+        case RUN: value = (Constants.Human.Female.SPEED + Constants.Human.Male.SPEED) / 2;
+        case SWIM: value = (Constants.Human.Female.SPEED + Constants.Human.Male.SPEED) / 4;
+    }
+    characterValue.setValue(value);
+    characterValue.setName(actType.name());
+    return characterValue;
   }
 
   @Override
   public CharacterValue getCharacterValue(ActType actType, Gender gender)
   {
-    //TODO
-    return null;
-
+    CharacterValue characterValue = new CharacterValue();
+    int value = 0;
+    switch (actType) {
+        case JUMP: value = (gender == Gender.MALE) ? Constants.Human.Male.POWER / 2: Constants.Human.Female.POWER / 5;
+        case RUN: value = (gender == Gender.MALE) ? Constants.Human.Male.SPEED / 3: Constants.Human.Female.SPEED / 3;
+        case SWIM: value = (gender == Gender.MALE) ? Constants.Human.Male.SPEED / 4: Constants.Human.Female.SPEED / 5;
+    }
+    characterValue.setValue(value);
+    characterValue.setName(actType.name());
+    return characterValue;
   }
 }
